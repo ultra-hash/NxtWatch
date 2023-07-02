2e403d11c70f39334ffa7b2d11d62e9760309585
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-02 08:48:39 +0000

    Added Highlights for the active menu item in the Navbar Component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,25 @@
 import {Component} from 'react'
 import {withRouter} from 'react-router-dom'
+import Popup from 'reactjs-popup'
 
 import {BiSun} from 'react-icons/bi'
 import {FaMoon} from 'react-icons/fa'
-import {NavbarContainer, AppLogoImg, ThemeButton} from './styledComponents'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {FiLogOut} from 'react-icons/fi'
+import {AiOutlineClose, AiFillHome} from 'react-icons/ai'
+import {HiFire} from 'react-icons/hi'
+import {SiYoutubegaming} from 'react-icons/si'
+import {MdPlaylistAdd} from 'react-icons/md'
+
+import {
+  NavbarContainer,
+  AppLogoImg,
+  Button,
+  CloseBtnContainer,
+  UnOrderedList,
+  ListItem,
+  MenuParagraphItem,
+} from './styledComponents'
 
 import ThemeContent from '../../context/themeContext'
 
@@ -20,6 +36,23 @@ class Navbar extends Component {
             toggleTheme()
           }
 
+          const openMenu = () => {
+            console.log('menu open')
+          }
+
+          const {match} = this.props
+          const {path} = match
+
+          const contentStyles = {
+            backgroundColor: isDarkTheme ? '#000000' : '#ffffff',
+            color: isDarkTheme ? '#ffffff' : '#000000',
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+          }
+
           return (
             <NavbarContainer dark={isDarkTheme}>
               <AppLogoImg
@@ -30,11 +63,96 @@ class Navbar extends Component {
                 }
                 alt="website logo"
               />
+              <div>
+                <Button type="button" onClick={onToggleTheme}>
+                  {isDarkTheme && <BiSun size={18} color="#ffffff" />}
+                  {!isDarkTheme && <FaMoon size={18} />}
+                </Button>
 
-              <ThemeButton type="button" onClick={onToggleTheme}>
-                {isDarkTheme && <BiSun size={18} color="#ffffff" />}
-                {!isDarkTheme && <FaMoon size={18} />}
-              </ThemeButton>
+                <Popup
+                  modal
+                  trigger={
+                    <Button type="button" onClick={openMenu}>
+                      <GiHamburgerMenu
+                        size={20}
+                        color={isDarkTheme ? '#ffffff' : '#000000'}
+                      />
+                    </Button>
+                  }
+                  position="center center"
+                  contentStyle={contentStyles}
+                >
+                  {close => {
+                    const getColorForMenuIcon = menuPath => {
+                      if (path === menuPath) {
+                        return '#ff0000'
+                      }
+                      if (isDarkTheme) {
+                        return '#ffffff'
+                      }
+                      return '#000000'
+                    }
+
+                    return (
+                      <>
+                        <CloseBtnContainer>
+                          <Button type="button" onClick={close}>
+                            <AiOutlineClose
+                              size={30}
+                              color={isDarkTheme ? '#ffffff' : '#000000'}
+                            />
+                          </Button>
+                        </CloseBtnContainer>
+                        <UnOrderedList>
+                          <ListItem dark={isDarkTheme} active={path === '/'}>
+                            <AiFillHome color={getColorForMenuIcon('/')} />{' '}
+                            <MenuParagraphItem dark={isDarkTheme}>
+                              Home
+                            </MenuParagraphItem>
+                          </ListItem>
+                          <ListItem
+                            dark={isDarkTheme}
+                            active={path === '/trending'}
+                          >
+                            <HiFire color={getColorForMenuIcon('/trending')} />{' '}
+                            <MenuParagraphItem dark={isDarkTheme}>
+                              Trending
+                            </MenuParagraphItem>
+                          </ListItem>
+                          <ListItem
+                            dark={isDarkTheme}
+                            active={path === '/gaming'}
+                          >
+                            <SiYoutubegaming
+                              color={getColorForMenuIcon('/gaming')}
+                            />{' '}
+                            <MenuParagraphItem dark={isDarkTheme}>
+                              Gaming
+                            </MenuParagraphItem>
+                          </ListItem>
+                          <ListItem
+                            dark={isDarkTheme}
+                            active={path === '/saved-videos'}
+                          >
+                            <MdPlaylistAdd
+                              color={getColorForMenuIcon('/saved-videos')}
+                            />{' '}
+                            <MenuParagraphItem dark={isDarkTheme}>
+                              Saved
+                            </MenuParagraphItem>
+                          </ListItem>
+                        </UnOrderedList>
+                      </>
+                    )
+                  }}
+                </Popup>
+                <Button type="button" onClick={openMenu}>
+                  <FiLogOut
+                    size={20}
+                    color={isDarkTheme ? '#ffffff' : '#000000'}
+                  />
+                </Button>
+              </div>
             </NavbarContainer>
           )
         }}
--- src/components/Navbar/styledComponents.js
+++ src/components/Navbar/styledComponents.js
@@ -11,8 +11,39 @@ export const NavbarContainer = styled.nav`
 export const AppLogoImg = styled.img`
   width: 100px;
 `
-export const ThemeButton = styled.button`
+export const Button = styled.button`
   background-color: transparent;
   border: 0;
   outline: none;
+`
+
+export const CloseBtnContainer = styled.div`
+  text-align: right;
+  padding: 20px;
+`
+
+export const UnOrderedList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: auto 0;
+`
+
+export const ListItem = styled.li`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+  color: ${props => (props.dark ? '#ffffff' : '#000000')};
+  background-color: ${props => {
+    const {active, dark} = props
+    if (active) {
+      return dark ? '#212121' : '#ebebeb'
+    }
+    return 'transparent'
+  }};
+`
+
+export const MenuParagraphItem = styled.p`
+  color: ${props => (props.dark ? '#ffffff' : '#000000')};
+  min-width: 60px;
 `
